93adcc8bd60ec6708b81f5e57c43d04de199fa3f
JK01JK <julianojk01@gmail.com>
2025-08-07 14:52:18 +0100

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/_jk01/workshopmongo/resources/UserResource.java
+++ src/main/java/com/_jk01/workshopmongo/resources/UserResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com._jk01.workshopmongo.domain.Post;
 import com._jk01.workshopmongo.domain.User;
 import com._jk01.workshopmongo.dto.UserDTO;
 import com._jk01.workshopmongo.services.UserService;
@@ -34,11 +35,7 @@ public class UserResource {
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
-		//System.out.println("ID que chegou no service: " + id);
-		//List<User> todos = service.findAll();
-		//System.out.println("IDs retornados pelo findAll():");
-		//todos.forEach(u -> System.out.println("  • " + u.getId()));
-		
+
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
@@ -64,6 +61,15 @@ public class UserResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
 
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
+	
+
+	
 	
 }
