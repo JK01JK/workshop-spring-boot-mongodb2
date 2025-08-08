98fc521053724429871e222ac5e8dd7e5d6d7f29
JK01JK <julianojk01@gmail.com>
2025-08-08 23:42:01 +0100

    Acrescentando comentarios aos posts

--- src/main/java/com/_jk01/workshopmongo/domain/Post.java
+++ src/main/java/com/_jk01/workshopmongo/domain/Post.java
@@ -1,13 +1,17 @@
 package com._jk01.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com._jk01.workshopmongo.dto.AuthorDTO;
+import com._jk01.workshopmongo.dto.CommentDTO;
 
 
 @Document
@@ -22,6 +26,8 @@ public class Post implements Serializable {
 	private String body;
 	private AuthorDTO author;
 	
+	private List<CommentDTO> comments = new ArrayList<>();
+	
 	public Post() {	
 	}
 
@@ -32,6 +38,15 @@ public class Post implements Serializable {
 		this.title = title;
 		this.body = body;
 		this.author = author;
+	}
+	
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 	public String getId() {
